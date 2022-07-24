f707b423e47c9f0dc2347ac4db05646b891a149a
dodongja <qs553@nate.com>
2022-07-24 12:49:26 +0900

    [이동혁] study

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -16,27 +16,28 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String api_key = "NCS1QRDDXPIDHH1Y";
-		String api_secret = "JMVREMDWHT58DCC22NT11OILTTRMYFVY";
-		Message coolsms = new Message(api_key, api_secret);
+		//만약에 2022-06-18 ~ 2022-06-20 까지 머무른다 치면 어떻게 검색이 되어야할까요?
+		//그럼 20022-06-18/2022-06-19 를 머물러야함
+		//그럼 06/18 , 06/19 가능한 날짜를 구해야함
 
-		// 4 params(to, from, type, text) are mandatory. must be filled
-		HashMap<String, String> params = new HashMap<String, String>();
-		params.put("to", "01039063340");
-		params.put("from", "01039063340");
-		params.put("type", "SMS");
-		params.put("text", "Coolsms Testing Message!");
-		params.put("app_version", "test app 1.2"); // application name and version
+		//일단 그럼 사업자는 날짜마다 방을 등록해야되나??
+		//만약에 객실1을 이용할 수 있는 날짜가 20220-06-18 ~ 2022-06-31 일이라치자
+		// roomNo  | check_in | check_out
+		//  1 	   | 2022-06-18| 2022-06-31 이라고 들어오면 만약에 중간에 방하나가 나가면 어떻게되는가??
+		//결국에는 날짜마다 객실 이용한 것이 다 들어가야되는가?
+		// 그럼 결국 등록할때 날짜 마다 객식 종류/ 객실 갯수를 등록해줘야함( 일할때 해봤는데 상당히 귀찮고 비효율적이라생각함)
+		//하지만 달리 방법이 생각 안남 ㅋ
+		// 그럼 결국 db에도 객실 종류/ 날짜/ 갯수 이렇게 들어가줘야하는건가??
+		//그럼 상세보기 페이지로 넘어가면 객실 종류의 정보가 들어오고 / 날짜 검색했을때 가능한 객실과 , 방의갯수가 업로드 되면 되는거네
+		// 배열로 만든다??
+		// 날짜당 이용할 수 있는 방은 그냥 다 하나 하나씩 db저장하나???
 
-		try {
-			JSONObject obj = (JSONObject) coolsms.send(params);
-			System.out.println(obj.toString());
-		} catch (CoolsmsException e) {
-			System.out.println(e.getMessage());
-			System.out.println(e.getCode());
-		}
+
+		//roomsetting 에는 방갯수 가격 날짜  3가지 요래하면 되려나
+		//그러면 날짜가 3박4일이다  첫쨋날 <= date < 마지막날 해서 나온것들
+		// 방갯수중최소값으로 해서 가능한 방 구하면될듯??
+		// 기본 1박은 그냥 그날짜 방갯수만 가져오면 됨
+		//
+
 	}
-
-
-
 }
---
+++ programmers/src/phoneketMon/Solution.java
@@ -0,0 +1,23 @@
+package phoneketMon;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int n = nums.length/2;
+
+        Set<Integer> monster = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            monster.add(nums[i]);
+        }
+        //monseter.size() 3이지만 n=2 이기 때문에 2
+        if(n >= monster.size()){
+            return answer = monster.size();
+        }else{
+            return answer = n;
+        }
+    }
+}
