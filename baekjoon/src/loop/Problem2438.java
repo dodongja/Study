8420f3991125e10c43cdb56b1fe3960b26dd6c5e
dodongja <qs553@nate.com>
2022-08-26 19:23:53 +0900

    [이동혁] 백준 10952번

--- baekjoon/src/loop/Problem2438.java
+++ baekjoon/src/loop/Problem2438.java
@@ -1,26 +1,18 @@
+package loop;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Problem2439 {
+public class Problem2438 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int one = 1;
-        String plusStar = "*";
+
         String star = "*";
-
-
-        for(int j = 0; j < n; j++) {
-            String answer = "";
-            String space = " ";
-            for (int i = one; i < n; i++) {
-                answer += space;
-            }
+        String answer = "";
+        for(int i = 0; i < n; i++){
             answer += star;
-            star += plusStar;
-            one++;
             System.out.println(answer);
         }
     }
