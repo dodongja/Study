8db02f1c2b56ef7d9399bdd71227cc941c53ef47
dodongja <qs553@nate.com>
2022-09-01 08:37:29 +0900

    [이동혁] 백준 8958번

--- baekjoon/src/array/Problem1546.java
+++ baekjoon/src/array/Problem1546.java
@@ -1,2 +1,35 @@
-package array;public class Problem1546 {
+package array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        float max = 0;
+        int[] score = new int[n];
+        float tmp = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            score[i] = Integer.parseInt(st.nextToken());
+            if(max < score[i]){
+                max = score[i];
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            tmp += score[i]/max*100;
+        }
+
+        System.out.println(tmp/n);
+
+
+
+    }
 }
